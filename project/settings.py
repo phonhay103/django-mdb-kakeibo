cb965a7b97c02c251913f4f630a1eeb0385f121b
unknown <phonhay103@gmail.com>
2022-07-19 11:08:26 +0700

    .

--- project/settings.py
+++ project/settings.py
@@ -137,10 +137,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')] #
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
